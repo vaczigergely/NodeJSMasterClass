d5d5f8593132bf2f58ea8da21f80e4c1617d3203
Gergely Vaczi <48059107+vaczigergely@users.noreply.github.com>
2021-08-22 19:32:35 +0200

    DELETE method

--- REST/lib/handlers.js
+++ REST/lib/handlers.js
@@ -115,7 +115,24 @@ handlers._users.put = function(data,callback) {
 };
 
 handlers._users.delete = function(data,callback) {
-    
+    const phone = typeof(data.queryStringObject.phone) == "string" && data.queryStringObject.phone.trim().length == 10 ? data.queryStringObject.phone.trim() : false;
+    if(phone) {
+        _data.read('users',phone,function(err,data) {
+            if(!err && data) {
+                _data.delete('users',phone,function(err) {
+                   if(!err) {
+                       callback(200);
+                   } else {
+                       callback(500,{ 'Error' : 'Could not delete specified user' });
+                   }
+                });
+            } else {
+                callback(400,{ 'Error' : 'Could not find the specified user' });
+            };
+        })
+    } else {
+        callback(400,{ 'Error' : 'Missing required field' });
+    };
 };
 
 
